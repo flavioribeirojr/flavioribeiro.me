25d92aec5247562753b755ae58b7845de2bde9d0
Flavio Ribeiro <flavioribeiro.dev@gmail.com>
2020-12-20 21:25:20 -0300

    Project setup

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Flavio Ribeiro | Desenvolvedor Fullstack</title>
+        <link rel="icon" href="/favicon.ico" />
+        <main>
+          <h1>Website on constrution</h1>
+        </main>
+      </Head>
+    </div>
+  )
+}
